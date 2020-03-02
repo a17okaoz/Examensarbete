ebfe4513cfca305eeccf782473831dacca182928
a17okaoz <a17okaoz@student.his.se>
2020-03-02 14:43:52 +0100

    jbj
    yaay

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 var inputValue = document.querySelector('.inputValue');
 var button = document.querySelector('.button');
 var name = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
+var country = document.querySelector('.country');
 
 button.addEventListener('click',function(){
   fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=00d7209b7ffda34cbe6e05e6f6746448')
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data =>{
+    var nameValue = data['name'];
+    var tempValue = data['main']['temp'];
+    var descValue = data['weather'][0]['description'];
+    var countryValue = data['sys']['country'];
+
+    tempValue = parseFloat(tempValue);
+
+    document.querySelector('.name').innerHTML = nameValue;
+    document.querySelector('.country').innerHTML = countryValue;
+    document.querySelector('.temp').innerHTML = tempValue-273.15+"℃";
+    document.querySelector('.desc').innerHTML = descValue;
+    console.log(data);
+  })
 })
