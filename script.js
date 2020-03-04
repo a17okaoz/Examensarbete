a1fef46888f36e82f13b608348cc58b18e47a477
a17okaoz <a17okaoz@student.his.se>
2020-03-04 14:10:08 +0100

    web application complete
    A 100% working weather application

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
-var inputValue = document.querySelector('.inputValue');
-var button = document.querySelector('.button');
-var name = document.querySelector('.name');
-var desc = document.querySelector('.desc');
-var temp = document.querySelector('.temp');
-var country = document.querySelector('.country');
+var searchbox = document.querySelector(".search-box");
+searchbox.addEventListener('keypress', setQuery);
 
-button.addEventListener('click',function(){
-  fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=00d7209b7ffda34cbe6e05e6f6746448')
-  .then(response => response.json())
-  .then(data =>{
-    var nameValue = data['name'];
-    var tempValue = data['main']['temp'];
-    var descValue = data['weather'][0]['description'];
-    var countryValue = data['sys']['country'];
+function setQuery(evt) {
+  if(evt.keyCode == 13) {
+    getWeatherResult(searchbox.value);
+  }
+}
 
-    tempValue = parseFloat(tempValue);
-
-    document.querySelector('.name').innerHTML = nameValue;
-    document.querySelector('.country').innerHTML = countryValue;
-    document.querySelector('.temp').innerHTML = tempValue-273.15+"℃";
-    document.querySelector('.desc').innerHTML = descValue;
-    console.log(data);
-  })
+var button = document.querySelector(".button");
+button.addEventListener("click", function(){
+  getWeatherResult(searchbox.value);
 })
+
+function getWeatherResult(query){
+  fetch('https://api.openweathermap.org/data/2.5/weather?q='+searchbox.value+'&appid=00d7209b7ffda34cbe6e05e6f6746448')
+  .then(weather =>{
+    return weather.json();
+  }).then(displayResults);
+}
+
+function displayResults(weather) {
+  console.log(weather);
+  var city = document.querySelector('.location .city');
+  city.innerHTML = `${weather.name}, ${weather.sys.country}`;
+
+  var today = new Date();
+  var date = document.querySelector(".location .date");
+  date.innerText = dateBuilder(today);
+
+  var temp = document.querySelector(".currentTemp .temp");
+  temp.innerHTML = `<img src="http://openweathermap.org/img/wn/${weather.weather[0].icon}.png" height="100" width="100"/> ${Math.round(weather.main.temp-273.15)}<span>℃</span>`;
+
+  var type = document.querySelector(".currentTemp .weatherType");
+  type.innerHTML = `${weather.weather[0].main}`;
+}
+
+function dateBuilder(todayDate) {
+  var month = ["januari", "februari", "mars", "april", "maj", "juni", "juli", "augusti",
+  "september", "oktober", "november", "december"];
+
+  var days = ["Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag"];
+
+  var day = days[todayDate.getDay()];
+  var date = todayDate.getDate();
+  var month = month[todayDate.getMonth()];
+  var year = todayDate.getFullYear();
+
+  var today = new Date();
+  var time = today.getHours() + ":" + today.getMinutes();
+
+
+return `${day} ${date} ${month} ${year} ${time}`;
+}
